364e3c0c14b6532a03595f47128add3fc278f79b
Mikaela <mikaela301@gmail.com>
2020-06-09 20:25:48 -0700

    Added POST route

--- app.js
+++ app.js
@@ -5,10 +5,19 @@ const request = require("request");
 const app = express();
 
 app.use(express.static("public")); 
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/signup.html")
 });
+
+app.post("/", function(req, res){
+    
+    let firstName = req.body.fName;
+    let lastName = req.body.lName;
+    let email = req.body.email;
+
+})
 
 app.listen(3000, function() {
     console.log("Server is running on port 3000...");

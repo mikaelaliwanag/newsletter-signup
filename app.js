741bac05c896c4b5d31b5f875e4b673b5bfd2fda
Mikaela <mikaela301@gmail.com>
2020-06-09 22:04:19 -0700

    Posted data to Mailchimp's servers via Mailchimp API

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
 
 const app = express();
 
@@ -12,13 +13,52 @@ app.get("/", function(req, res){
 });
 
 app.post("/", function(req, res){
-    
-    let firstName = req.body.fName;
-    let lastName = req.body.lName;
-    let email = req.body.email;
 
-})
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
+
+    const data = {
+       members: [
+            {
+              email_address: email,
+              status: "subscribed",
+              merge_fields: {
+                  FNAME: firstName,
+                  LNAME: lastName
+              }  
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+    
+    const url = "https://us10.api.mailchimp.com/3.0/lists/fef0ef47d3"
+
+    const options = {
+        method: "POST",
+        auth: "mikaela:be3b95c1b31ea1fee39a3150d55939d4-us10"
+    }
+
+    const request = https.request(url, options, function(response) {
+       if(response.statusCode === 200 ) {
+           res.send("Successfully subscribed!");
+       } else {
+           res.send("There was an eror.")
+       }
+
+        response.on("data", function(data){
+             console.log(JSON.parse(data));
+         })
+    })
+
+    request.write(jsonData);
+    request.end();
+
+});
 
 app.listen(3000, function() {
     console.log("Server is running on port 3000...");
 });
+
+
